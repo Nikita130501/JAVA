3c75f4e460cbfc9f969a089b8bc3f6da30b391e4
root <root@3636943-uj76113.twc1.net>
2024-10-18 13:25:25 +0300

    Updated init.sql to fix table creation order

--- init.sql
+++ init.sql
@@ -1,7 +1,13 @@
+-- Сначала создаем таблицу car_brands
+CREATE TABLE IF NOT EXISTS car_brands (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    brand_name VARCHAR(255) NOT NULL
+);
+
+-- Затем создаем таблицу car_models, которая ссылается на car_brands
 CREATE TABLE IF NOT EXISTS car_models (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     model_name VARCHAR(255) NOT NULL,
     brand_id BIGINT,
     CONSTRAINT FK_brand FOREIGN KEY (brand_id) REFERENCES car_brands(id)
 );
-
